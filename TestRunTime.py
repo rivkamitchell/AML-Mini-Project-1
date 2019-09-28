e86a568b83f5883c6233a4b5f10e7babadc4102f
HarrisonMcDonough <39010115+HarrisonMcDonough@users.noreply.github.com>
2019-09-28 16:55:10 -0400

    Create TestRunTime.py

---
+++ TestRunTime.py
@@ -0,0 +1,11 @@
+import time
+
+start_time = time.time()
+
+#the start of the activity you wish to time
+
+#stuff happens
+
+#the end of the activity you wish to time
+print("--- %s seconds ---" % (time.time() - start_time))
+
